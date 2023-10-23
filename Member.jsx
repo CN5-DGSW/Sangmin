cd49e3bc7ef70dde63f751b587b334cc3a239973
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2023-10-23 23:14:39 +0900

    Add files via upload

--- Member.jsx
+++ Member.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Member.css';
 import poingLogo from "../src/img/poingLogo.svg";
+import './Login'
 
 const Login = () => {
   const [nameValue, setNameValue] = useState("");
@@ -14,10 +15,10 @@ const Login = () => {
   };
 
   return (
-    <div className='MainLayout'>
+    <div className='MainMember'>
       <img src={poingLogo} alt="Poing Logo" />
       <p className='Yellow sendToTeacher'>선생님께 마음을 전할 준비가 됐나요?</p>
-      <div className='InputWrap'>
+      <div className='InputMember'>
         <input
           className='Input'
           type='text'
@@ -26,7 +27,7 @@ const Login = () => {
           onChange={(e) => setNameValue(e.target.value)}
         />
       </div>
-      <div className='InputWrap'>
+      <div className='InputMember'>
         <input
           className='Input'
           type='password'
@@ -34,29 +35,28 @@ const Login = () => {
           value={pwValue}
           onChange={(e) => setPwValue(e.target.value)}
         />
-        <div className='InputWrap'>
-            <input
-            className='Input'
-            type='text'
-            placeholder='학년, 반, 번호을 입력하세요.   ex)1413'
-            value={classValue}
-            onChange={(e) => setClassValue(e.target.value)}
-            />
-        </div>
-        <div className='InputWrap'>
-            <input
-            className='Input'
-            type='password'
-            placeholder='비밀번호를 입력하세요.'
-            value={emailValue}
-            onChange={(e) => setEmailValue(e.target.value)}
-            />
-        </div>
       </div>
-      <button className='LoginBtn Yellow' onClick={onClickHandler}>
-        로그인
+      <div className='InputMember'>
+        <input
+          className='Input'
+          type='text'
+          placeholder='학년, 반, 번호을 입력하세요.   ex)1413'
+          value={classValue}
+          onChange={(e) => setClassValue(e.target.value)}
+        />
+      </div>
+      <div className='InputMember'>
+        <input
+          className='Input'
+          type='password'
+          placeholder='이메일을 입력하세요.'
+          value={emailValue}
+          onChange={(e) => setEmailValue(e.target.value)}
+        />
+      </div>
+      <button className='MemberBtn Yellow' onClick={onClickHandler}>
+        회원가입
       </button>
-      <p className='Yellow Under'>회원가입</p>
     </div>
   );
 };
