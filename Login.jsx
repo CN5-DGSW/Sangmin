dc42605eae180e2794cbfdd1fad15457d91168dd
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2023-10-29 16:42:11 +0900

    Add files via upload

--- Login.jsx
+++ Login.jsx
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import './Member';
 import poingLogo from "../src/img/poingLogo.svg";
+import './Teacher';
+import axios from 'axios';
 
 const Login = () => {
   const [emailValue, setEmailValue] = useState("");
   const [pwValue, setPwValue] = useState("");
+  const Navigate = useNavigate();
 
-  const onClickHandler = () => {
-    console.log("Id : " + emailValue, "Password : " + pwValue);
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get(`로그인정보 확인하는 서버`);
+      console.log(response.data); // 정보 출력하기
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const response = await axios.post('내가 보내줄 서버', {
+        email: emailValue,
+        password: pwValue
+      });
+      if (response.status === 200) {
+        fetchUserInfo(); // 로그인 되면 사용자 정보 받기
+        Navigate("/Teacher");
+      } else {
+        alert('로그인 정보가 일치하지 않습니다.')
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div className='MainLayout'>
       <img src={poingLogo} alt="Poing Logo" />
-      <p className='Yellow sendToTeacher'>선생님께 마음을 전할 준비가 됐나요?</p>
+      <p className='Yellow sendToTeacher'>선생님께 마음을 전할 준비가 되었나요?</p>
       <div className='InputWrap'>
         <input
           className='Input'
@@ -33,11 +59,11 @@ const Login = () => {
           onChange={(e) => setPwValue(e.target.value)}
         />
       </div>
-      <button className='LoginBtn Yellow' onClick={onClickHandler}>
+      <button className='LoginBtn Yellow' onClick={login}>
         로그인
       </button>
       <div>
-        <p className='Yellow Under' onClick={() => Navigate("/Member")}>
+        <p className='Yellow Under' onClick={() => {Navigate("/member")}}>
           회원가입
         </p>
       </div>
--- Router.js
+++ Router.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Login from './Login'
 import Member from './Member'
+import Teacher from './Teacher'
 
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Login />
       <Routes>
-          <Route path='/Login' element={<Login />} />
+          <Route path='/' element={<Login />} />
           <Route path='/Member' element={<Member />} />
+          <Route path='/Teacher' element={<Teacher />} />
       </Routes>
     </BrowserRouter>
   )
